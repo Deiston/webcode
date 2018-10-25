ca9d8f150594b304bcf3c2d14229f9c12319951d
HUI LEE <36818242+aclihui@users.noreply.github.com>
2018-10-25 15:24:45 +0800

    add time.c
    add time.c

---
+++ time.c
@@ -0,0 +1,20 @@
+//毫秒计时函数 
+#include<stdio.h>
+#include <stdlib.h>//用到rand()函数
+#include<time.h>   //用到clock()函数
+int main() {
+	int begintime,endtime;
+	int i = 0;
+	int a[1002];
+	begintime=clock();	//计时开始
+	printf("\n%dms\n",clock());
+	for( i = 1; i <= 1000; i++){//要计时的程序
+		a[i] = rand()%200-100;//产生-100到+100之间的随机数
+		printf("  %d",a[i]);
+	}
+	endtime = clock();	//计时结束
+	
+	printf("\n%dms\n",clock()); 
+	printf("\n\nRunning Time：%dms\n", endtime-begintime);
+	return 0;
+}
